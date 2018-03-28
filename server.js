65bafebb7c75fb4d24f2f5d1d69ff28a284bff7b
David Looi <david@cultureamp.com>
2018-03-29 09:15:04 +1100

    Add mutation

--- server.js
+++ server.js
@@ -12,6 +12,23 @@ var schema = buildSchema(`
 
   type Query {
     getDie(numSides: Int): RandomDie
+    getMessage(id: ID!): Message
+  }
+
+  input MessageInput {
+    content: String
+    author: String
+  }
+
+  type Message {
+    id: ID!
+    content: String
+    author: String
+  }
+
+  type Mutation {
+    createMessage(input: MessageInput): Message
+    updateMessage(id: ID!, input: MessageInput): Message
   }
 `);
 
@@ -34,10 +51,42 @@ class RandomDie {
   }
 }
 
+// If Message had any complex fields, we'd put them on this object.
+class Message {
+  constructor(id, {content, author}) {
+    this.id = id;
+    this.content = content;
+    this.author = author;
+  }
+}
+
+var fakeDatabase = {};
+
 // The root provides the top-level API endpoints
 var root = {
   getDie: function ({numSides}) {
     return new RandomDie(numSides || 6);
+  },
+  getMessage: function ({id}) {
+    if (!fakeDatabase[id]) {
+      throw new Error('no message exists with id ' + id);
+    }
+    return new Message(id, fakeDatabase[id]);
+  },
+  createMessage: function ({input}) {
+    // Create a random id for our "database".
+    var id = require('crypto').randomBytes(10).toString('hex');
+
+    fakeDatabase[id] = input;
+    return new Message(id, input);
+  },
+  updateMessage: function ({id, input}) {
+    if (!fakeDatabase[id]) {
+      throw new Error('no message exists with id ' + id);
+    }
+    // This replaces all old data, but some apps might want partial update.
+    fakeDatabase[id] = input;
+    return new Message(id, input);
   }
 }
 
